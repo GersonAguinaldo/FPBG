7664277e110d50e307e665b2e7a84bba3136c4b7
devgroup-enterprise <devgroupentreprise@gmail.com>
2025-09-28 03:10:05 +0100

    configuration de la partie front (user) : 1- page acceuil 2- sign up et  login 3- dashboard 4- formulaire

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,27 @@
-// src/app/app.ts
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, effect } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/auth.service';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('FPBG');
-  auth = inject(AuthService); // <- permet *ngIf="!auth.isLoggedIn()" dans app.html
+  auth = inject(AuthService);
+  private router = inject(Router);
+
+  // pages qui doivent exploiter toute la largeur
+  private wideMatcher = (url: string) => /\/(form|user\/form|submission)/.test(url);
+
+  wide = signal<boolean>(this.wideMatcher(this.router.url));
+  constructor() {
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => this.wide.set(this.wideMatcher(this.router.url)));
+  }
 }
